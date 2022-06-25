7e67795065d9211c7a0bc9e7f5073d4d9495a483
fathurizqillah <rizqillahfathu@gmail.com>
2022-06-25 16:40:20 +0700

    pengelolan pengguna

---
+++ src/app/admin/pengguna/dialog-update-password/dialog-update-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-update-password',
+  templateUrl: './dialog-update-password.component.html',
+  styleUrls: ['./dialog-update-password.component.scss']
+})
+export class DialogUpdatePasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/admin/pengguna/pengguna.component.ts
+++ src/app/admin/pengguna/pengguna.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogSettingRoleComponent } from './dialog-setting-role/dialog-setting-role.component';
+import { DialogUpdatePasswordComponent } from './dialog-update-password/dialog-update-password.component';
+import { DialogUserComponent } from './dialog-user/dialog-user.component';
 
 @Component({
   selector: 'app-pengguna',
@@ -16,9 +18,29 @@ export class PenggunaComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //Dialog Tambah/Edit Pengguna
+  dialogPengguna(): void {
+    const dialogRef = this.dialog.open(DialogUserComponent, {
+      width: '650px',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
   //Dialog Edit Role
   editRole(): void {
     const dialogRef = this.dialog.open(DialogSettingRoleComponent, {
+      width: '650px',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  //Dialog Edit Password
+  updatePassword(): void {
+    const dialogRef = this.dialog.open(DialogUpdatePasswordComponent, {
       width: '650px',
     });
     dialogRef.afterClosed().subscribe(result => {
